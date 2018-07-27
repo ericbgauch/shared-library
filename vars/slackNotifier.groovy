cb238eb3f80abd01eddf7808abebd0d2ffd119cf
Eric Bueno Gauch <ericbgauch@gmail.com>
2018-07-27 18:24:29 -0300

    test new notification message

--- vars/slackNotifier.groovy
+++ vars/slackNotifier.groovy
@@ -1,8 +1,8 @@
 #!/usr/bin/env groovy
 
-def call(String buildResult, String ch) {
+def call(String buildResult, String ch, String branch, String commit, String author) {
   if ( buildResult == "SUCCESS" ) {
-    slackSend channel: ch, color: "good", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was successful"
+    slackSend channel: ch, color: "good", message: "Build ${env.BUILD_NUMBER} de ${env.JOB_NAME} \nbranch: ${branch} \ncommit: ${commit} \nautor: ${author} \nSUCESSO"
   }
   else if( buildResult == "FAILURE" ) { 
     slackSend channel: ch, color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was failed"
